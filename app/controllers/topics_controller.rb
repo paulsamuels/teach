5e0e6d7310efe58650b944c6d31ab552fba2b11d
Paul Samuels <paulio1987@gmail.com>
2011-09-03 18:27:52 +0100

    More styling and added pagination

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,7 @@ class TopicsController < ApplicationController
   # GET /topics
   # GET /topics.xml
   def index
-    @topics = Topic.root_topics
+    @topics = Topic.root_topics.paginate(:page => params[:page], :per_page => 8)
 
     respond_to do |format|
       format.html # index.html.erb
