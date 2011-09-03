cf16a10600aece0edd3bffe2c58f538ac362f3e3
Paul Samuels <paulio1987@gmail.com>
2011-09-03 17:07:00 +0100

    Added jquery and more basic styling of view of Topics

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+    $('a.detail_tab').click(function() {
+      $('li.links_tab').removeClass('selected');
+      $('li.detail_tab').addClass('selected');
+      $('#topic_detail').show();
+      $('#topic_links').hide();
+    })
+    $('a.links_tab').click(function() {
+      $('li.links_tab').addClass('selected');
+      $('li.detail_tab').removeClass('selected');
+      $('#topic_detail').hide();
+      $('#topic_links').show();
+    })
+})
